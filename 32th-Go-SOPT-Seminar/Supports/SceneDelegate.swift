8e20184a7f7626404818d46a7d04d5245500771e
Joon Baek <joonbaek12@gmail.com>
2023-04-19 20:26:28 +0900

    [Feat] #3 - Add fonts and assets

--- 32th-Go-SOPT-Seminar/Supports/SceneDelegate.swift
+++ 32th-Go-SOPT-Seminar/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let injectViewController = Inject.ViewControllerHost(AutoLayoutPracticeViewController())
+        let injectViewController = Inject.ViewControllerHost(SignInViewController())
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = injectViewController
         self.window?.makeKeyAndVisible()
