cd03faa4d31b22d7c055a8d78f85449b0cf49738
Joon Baek <joonbaek12@gmail.com>
2023-04-19 20:23:25 +0900

    [Feat] #3 - Add font, color, image literals

---
+++ 32th-Go-SOPT-Seminar/Tving-iOS/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,23 @@
+//
+//  ImageLiterals.swift
+//  32th-Go-SOPT-Seminar
+//
+//  Created by Joon Baek on 2023/04/18.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct Image {
+        static let logo = UIImage(named: "tving-main")
+    }
+    
+    struct Icn {
+        static let back = UIImage(named: "btn_before")
+        static let hidden = UIImage(named: "security-on")
+        static let securityOn = UIImage(named: "security-On")
+        static let securityOff = UIImage(named: "security-off")
+        static let clear = UIImage(named: "x-circle")
+    }
+}
